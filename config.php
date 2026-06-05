8ee1314b2b287094c9fa079cb38de287c41be590
Maja Pytowska <mpyt@pixel-tech.pl>
2026-06-05 13:49:06 +0200

    - git pull to Vm test V1

--- config.php
+++ config.php
@@ -2,16 +2,16 @@
 $conf->debug = true; # set true during development and use in your code (for instance check if true to send additional message)
 
 # ---- Webapp location
-$conf->server_name = 'localhost';   # server address and port
+$conf->server_name = $_SERVER['HTTP_HOST'];   # server address and port
 $conf->protocol = 'http';           # http or https
-$conf->app_root = '/clinic-appointment-system/public';   # project subfolder in domain (relative to main domain)
+$conf->app_root = '/public';   # project subfolder in domain (relative to main domain)
 
 # ---- Database config - values required by Medoo
 $conf->db_type = 'mysql';
-$conf->db_server = 'localhost';
-$conf->db_name = 'clinicappointmentssystem';
+$conf->db_server = 'db';
+$conf->db_name = getenv('DB_NAME');
 $conf->db_user = 'root';
-$conf->db_pass = '';
+$conf->db_pass = getenv('DB_ROOT_PASS');
 $conf->db_charset = 'utf8';
 
 # ---- Database config - optional values
